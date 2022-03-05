4a046678f834651bdad9719cbaa0093d0a68e0fa
Royal Chen <indifferencedead@yahoo.com>
2022-03-05 13:19:47 +0800

    Add useDateOption hook and handleDateOption method

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
 import styles from "./style.module.scss";
 import classnames from "classnames/bind";
 
+import { useDateOption } from "@/hooks/useDateOption";
+
 const cx = classnames.bind(styles);
 
 export default function Home() {
+  const { yearOptions, monthOptions, dayOptions } = useDateOption({
+    year: "2021",
+    month: "7",
+  });
+
+  console.log("yearOptions", yearOptions);
+  console.log("monthOptions", monthOptions);
+  console.log("dayOptions", dayOptions);
+
   return <div className={cx("home")}>aaa</div>;
 }
