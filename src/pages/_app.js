37cc94c8159ee23774e74c44809e8a72be47dbd1
Royal Chen <indifferencedead@yahoo.com>
2023-06-11 22:18:27 +0800

    Add redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,26 +1,16 @@
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 import { appWithTranslation } from "next-i18next";
 import "core-js";
-// import "@babel/polyfill";
-
-import App from "@/components/App";
+import store from "@/redux/store";
+import { Provider } from "react-redux";
 
 import "../styles/globals.scss";
 
-Sentry.init({
-  dsn: "https://f7242f65f6664800b5c502c10f4fe56c@o1189518.ingest.sentry.io/6310267",
-  integrations: [new BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-  environment: "test",
-});
-
-function MyApp(props) {
-  return <App {...props} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default appWithTranslation(MyApp);
---
+++ src/redux/reducer/room.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const roomSlice = createSlice({
+  name: "room",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { increment } = roomSlice.actions;
+
+export default roomSlice.reducer;
